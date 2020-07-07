faa80a557d4c6594696f45c74e3200a61060d51a
Vlad <acv4acc@gmail.com>
2020-07-07 11:45:56 +0100

    6.1

--- problem_mapping.js
+++ problem_mapping.js
@@ -892,7 +892,7 @@ problem_mapping = {
                 "total": 108
             },
             "Java: EvaluateRpn.java": {
-                "passed": 0,
+                "passed": 108,
                 "total": 108
             },
             "Python: evaluate_rpn.py": {
